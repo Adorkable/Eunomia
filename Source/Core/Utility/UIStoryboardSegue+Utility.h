8f878a1b699c79f8410f135a22e412b58c7b9c1d
Ian G <yo.ian.g@gmail.com>
2014-09-05 00:26:34 -0400

    transferSubview animation support

--- Source/Core/Utility/UIStoryboardSegue+Utility.h
+++ Source/Core/Utility/UIStoryboardSegue+Utility.h
@@ -18,11 +18,32 @@
 
 - (void)notifySegueFinishedAnimating:(id)object;
 
+- (void)doTransferSubviewPrep;
+- (void)doTransferSubviewDestinationFrame;
+- (void)doTransferSubviewCompleteAnimation;
+
 @end
 
 @protocol AnimatedSegueViewController <NSObject>
 
 @optional
 - (void)segueFinishedAnimating:(UIStoryboardSegue *)segue;
+
+@end
+
+@protocol ReplaceSegueSourceViewController <NSObject>
+
+@optional
+- (BOOL)replaceSegueSourceShouldTransferSubview:(UIStoryboardSegue *)segue;
+- (UIView *)replaceSegueSourceTransferSubview:(UIStoryboardSegue *)segue;
+
+@end
+
+@protocol ReplaceSegueDestinationViewController <NSObject>
+
+@optional
+- (BOOL)replaceSegueDestinationShouldTransferSubview:(UIStoryboardSegue *)segue;
+- (CGRect)replaceSegueDestinationTransferSubviewDestinationRect:(UIStoryboardSegue *)segue;
+- (UIView *)replaceSegueDestinationTransferSubviewDestinationRectRelativeView:(UIStoryboardSegue *)segue;
 
 @end
--- Source/Core/Utility/UIStoryboardSegue+Utility.m
+++ Source/Core/Utility/UIStoryboardSegue+Utility.m
@@ -10,6 +10,27 @@
 
 #import "UIViewController+Utility.h"
 
+#import "Eunomia.h"
+#import <objc/runtime.h>
+
+NSString *const Eunomia_Utility_UIStoryboardSegue_DoTransferSubviewKey = @"Eunomia_Utility_UIStoryboardSegue_DoTransferSubview";
+NSString *const Eunomia_Utility_UIStoryboardSegue_TransferSubviewKey = @"Eunomia_Utility_UIStoryboardSegue_TransferSubview";
+NSString *const Eunomia_Utility_UIStoryboardSegue_TransferSubviewLeftKey = @"Eunomia_Utility_UIStoryboardSegue_TransferSubviewLeft";
+NSString *const Eunomia_Utility_UIStoryboardSegue_TransferSubviewTopKey = @"Eunomia_Utility_UIStoryboardSegue_TransferSubviewTop";
+NSString *const Eunomia_Utility_UIStoryboardSegue_TransferSubviewWidthKey = @"Eunomia_Utility_UIStoryboardSegue_TransferSubviewWidth";
+NSString *const Eunomia_Utility_UIStoryboardSegue_TransferSubviewHeightKey = @"Eunomia_Utility_UIStoryboardSegue_TransferSubviewHeight";
+
+@interface UIStoryboardSegue (Eunomia_Utility_Private)
+
+@property (readwrite) BOOL doTransferSubview;
+@property (weak, readwrite, getter=getTransferSubview) UIView *transferSubview;
+@property (strong, readwrite, getter=getTransferSubviewLeft) NSLayoutConstraint *transferSubviewLeft;
+@property (strong, readwrite, getter=getTransferSubviewTop) NSLayoutConstraint *transferSubviewTop;
+@property (strong, readwrite, getter=getTransferSubviewWidth) NSLayoutConstraint *transferSubviewWidth;
+@property (strong, readwrite, getter=getTransferSubviewHeight) NSLayoutConstraint *transferSubviewHeight;
+
+@end
+
 @implementation UIStoryboardSegue (Eunomia_Utility)
 
 + (UIViewController *)safelyCastToViewController:(id)object
@@ -83,5 +104,161 @@
         }
     }
 }
+
+#pragma mark ReplaceSegueViewController
+
+- (void)doTransferSubviewPrep
+{
+    self.doTransferSubview = NO;
+    
+    BOOL sourceShouldTransfer = NO;
+    id<ReplaceSegueSourceViewController> transferSource;
+    if ( [self.sourceViewController conformsToProtocol:@protocol(ReplaceSegueSourceViewController) ] )
+    {
+        transferSource = (id<ReplaceSegueSourceViewController>)self.sourceViewController;
+        
+        sourceShouldTransfer = YES;
+        if ( [transferSource respondsToSelector:@selector(replaceSegueSourceShouldTransferSubview:) ] )
+        {
+            sourceShouldTransfer = [transferSource replaceSegueSourceShouldTransferSubview:self];
+        }
+    }
+    
+    BOOL destinationShouldTransfer = NO;
+    id<ReplaceSegueDestinationViewController> transferDestination;
+    if ( [self.destinationViewController conformsToProtocol:@protocol(ReplaceSegueDestinationViewController) ] )
+    {
+        transferDestination = (id<ReplaceSegueDestinationViewController>)self.destinationViewController;
+
+        destinationShouldTransfer = YES;
+        if ( [transferDestination respondsToSelector:@selector(replaceSegueDestinationShouldTransferSubview:) ] )
+        {
+            destinationShouldTransfer = [transferDestination replaceSegueDestinationShouldTransferSubview:self];
+        }
+    }
+    
+    UIView *destinationView = self.destinationViewControllerTyped.view;
+    
+    if (sourceShouldTransfer && destinationShouldTransfer && destinationView)
+    {
+        if ( [transferSource respondsToSelector:@selector(replaceSegueSourceTransferSubview:) ] )
+        {
+            self.transferSubview = [transferSource replaceSegueSourceTransferSubview:self];
+            
+            if (self.transferSubview &&
+                [transferDestination respondsToSelector:@selector(replaceSegueDestinationTransferSubviewDestinationRect:) ] &&
+                [transferDestination respondsToSelector:@selector(replaceSegueDestinationTransferSubviewDestinationRectRelativeView:) ] )
+            {
+                [destinationView layoutSubviews];
+                
+                //                CGPoint relativeOrigin = [destinationView convertPoint:self.transferSubview.frame.origin fromView:self.transferSubview.superview];
+                CGRect relativeStartFrame = //CGRectMake(relativeOrigin.x, relativeOrigin.y, self.transferSubview.frame.size.width, self.transferSubview.frame.size.height);
+                [destinationView convertRect:self.transferSubview.frame fromView:self.transferSubview.superview];
+                [self.transferSubview removeFromSuperview];
+                
+                [destinationView addSubview:self.transferSubview];
+                
+                self.transferSubviewLeft = [NSLayoutConstraint constraintWithItem:self.transferSubview
+                                                                        attribute:NSLayoutAttributeLeft
+                                                                        relatedBy:NSLayoutRelationEqual
+                                                                           toItem:destinationView
+                                                                        attribute:NSLayoutAttributeLeft
+                                                                       multiplier:1.0 constant:relativeStartFrame.origin.x];
+                [destinationView addConstraint:self.transferSubviewLeft];
+                
+                self.transferSubviewTop = [NSLayoutConstraint constraintWithItem:self.transferSubview
+                                                                       attribute:NSLayoutAttributeTop
+                                                                       relatedBy:NSLayoutRelationEqual
+                                                                          toItem:destinationView
+                                                                       attribute:NSLayoutAttributeTop
+                                                                      multiplier:1.0 constant:relativeStartFrame.origin.y];
+                [destinationView addConstraint:self.transferSubviewTop];
+                
+                self.transferSubviewWidth = [NSLayoutConstraint constraintWithItem:self.transferSubview
+                                                                         attribute:NSLayoutAttributeWidth
+                                                                         relatedBy:NSLayoutRelationEqual
+                                                                            toItem:nil
+                                                                         attribute:NSLayoutAttributeNotAnAttribute
+                                                                        multiplier:1.0 constant:relativeStartFrame.size.width];
+                [destinationView addConstraint:self.transferSubviewWidth];
+                
+                self.transferSubviewHeight = [NSLayoutConstraint constraintWithItem:self.transferSubview
+                                                                          attribute:NSLayoutAttributeHeight
+                                                                          relatedBy:NSLayoutRelationEqual
+                                                                             toItem:nil
+                                                                          attribute:NSLayoutAttributeNotAnAttribute
+                                                                         multiplier:1.0 constant:relativeStartFrame.size.height];
+                [destinationView addConstraint:self.transferSubviewHeight];
+                
+                [destinationView layoutIfNeeded];
+                
+                self.doTransferSubview = YES;
+            }
+        }
+    }
+}
+
+- (void)doTransferSubviewDestinationFrame
+{
+    if (self.doTransferSubview)
+    {
+        id<ReplaceSegueSourceViewController> replaceLeftSource;
+        if ( [self.sourceViewController conformsToProtocol:@protocol(ReplaceSegueSourceViewController) ] )
+        {
+            replaceLeftSource = (id<ReplaceSegueSourceViewController>)self.sourceViewController;
+        }
+        
+        id<ReplaceSegueDestinationViewController> replaceLeftDestination;
+        if ( [self.destinationViewController conformsToProtocol:@protocol(ReplaceSegueDestinationViewController) ] )
+        {
+            replaceLeftDestination = (id<ReplaceSegueDestinationViewController>)self.destinationViewController;
+        }
+        
+        UIView *destinationView = self.destinationViewControllerTyped.view;
+        CGRect transferSubviewDestinationRect = [replaceLeftDestination replaceSegueDestinationTransferSubviewDestinationRect:self];
+        UIView *transferSubviewDestinationRectRelativeView = [replaceLeftDestination replaceSegueDestinationTransferSubviewDestinationRectRelativeView:self];
+        
+        CGRect transferDestinationRect = [destinationView convertRect:transferSubviewDestinationRect fromView:transferSubviewDestinationRectRelativeView];
+        
+        self.transferSubviewLeft.constant = transferDestinationRect.origin.x;
+        self.transferSubviewTop.constant = transferDestinationRect.origin.y;
+        self.transferSubviewWidth.constant = transferDestinationRect.size.width;
+        self.transferSubviewHeight.constant = transferDestinationRect.size.height;
+        
+        [destinationView layoutIfNeeded];
+    }
+}
+
+- (void)doTransferSubviewCompleteAnimation
+{
+    if (self.doTransferSubview)
+    {
+        [self.transferSubview removeFromSuperview];
+        self.transferSubview = nil;
+    }
+}
+
+- (void)setDoTransferSubview:(BOOL)doTransferSubview
+{
+    SetProtocolProperty( [NSNumber numberWithBool:doTransferSubview], Eunomia_Utility_UIStoryboardSegue_DoTransferSubviewKey);
+}
+
+- (BOOL)doTransferSubview
+{
+    BOOL result = NO;
+    id object = GetProtocolProperty(Eunomia_Utility_UIStoryboardSegue_DoTransferSubviewKey);
+    if ( [object isKindOfClass:[NSNumber class] ] )
+    {
+        NSNumber *number = object;
+        result = [number boolValue];
+    }
+    return result;
+}
+
+DefineProtocolProperty(UIView, TransferSubview, Eunomia_Utility_UIStoryboardSegue_TransferSubviewKey);
+DefineProtocolProperty(NSLayoutConstraint, TransferSubviewLeft, Eunomia_Utility_UIStoryboardSegue_TransferSubviewLeftKey);
+DefineProtocolProperty(NSLayoutConstraint, TransferSubviewTop, Eunomia_Utility_UIStoryboardSegue_TransferSubviewTopKey);
+DefineProtocolProperty(NSLayoutConstraint, TransferSubviewWidth, Eunomia_Utility_UIStoryboardSegue_TransferSubviewWidthKey);
+DefineProtocolProperty(NSLayoutConstraint, TransferSubviewHeight, Eunomia_Utility_UIStoryboardSegue_TransferSubviewHeightKey);
 
 @end
