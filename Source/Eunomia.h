f65fd11b3e864b30bf401c5343fb1454007a5374
Ian G <yo.ian.g@gmail.com>
2014-09-05 00:26:22 -0400

    protocol property macros
    TODO: find a better place

--- Source/Eunomia.h
+++ Source/Eunomia.h
@@ -10,10 +10,33 @@
 
 #import "Config.h"
 
+#define SetProtocolProperty(value, key) \
+    objc_setAssociatedObject(self, CFBridgingRetain(key), value, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+#define GetProtocolProperty(key) \
+    objc_getAssociatedObject(self, CFBridgingRetain(key) )
+
+// Cannot use with non-pointer types
+#define DefineProtocolProperty(Type, Name, key) \
+- (void)set##Name:(Type *)value \
+{ \
+    SetProtocolProperty(value, key); \
+} \
+\
+- (Type *)get##Name \
+{ \
+    Type *result; \
+    id object = GetProtocolProperty(key); \
+    if ( [object isKindOfClass:[Type class] ] ) \
+    { \
+        result = object; \
+    } \
+    return result; \
+}
+
 #import "CoreData.h"
 #import "Exceptions.h"
+#import "Utility.h"
 #import "ThirdParty.h"
 #import "Segues.h"
-#import "Utility.h"
 #import "Views.h"
 #import "View+Templating.h"
