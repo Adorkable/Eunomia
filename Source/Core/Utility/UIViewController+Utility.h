14197d8bb874abde2201ee01ba54bef998f917d8
Ian G <yo.ian.g@gmail.com>
2014-10-20 15:15:21 -0400

    move old WeAreAllTextSoAssignUs prototyping into Eunomia

--- Source/Core/Utility/UIViewController+Utility.h
+++ Source/Core/Utility/UIViewController+Utility.h
@@ -17,3 +17,30 @@
             andRootViewController:(BOOL)replaceRootViewController;
 
 @end
+
+@interface UIViewController (Eunomia_Utility_WeAreAllTextSoAssignUs)
+
+- (void)mapViews:(NSArray *)views forKey:(NSString *)key;
+- (void)mapLabel:(UILabel *)label
+       andButton:(UIButton *)button
+    andTextField:(UITextField *)textField
+     andTextView:(UITextView *)textView
+          forKey:(NSString *)key;
+
+- (void)setText:(NSString *)text toViews:(NSArray *)views;
+- (void)setText:(NSString *)text toViewsWithKey:(NSString *)key;
+- (void)setText:(NSString *)text
+        toLabel:(UILabel *)label
+      andButton:(UIButton *)button
+   andTextField:(UITextField *)textField
+    andTextView:(UITextView *)textView;
+
+- (void)setHidden:(BOOL)hidden toViews:(NSArray *)views;
+- (void)setHidden:(BOOL)hidden toViewsWithKey:(NSString *)key;
+- (void)setHidden:(BOOL)hidden
+          toLabel:(UILabel *)label
+        andButton:(UIButton *)button
+     andTextField:(UITextField *)textField
+      andTextView:(UITextView *)textView;
+
+@end
--- Source/Core/Utility/UIViewController+Utility.m
+++ Source/Core/Utility/UIViewController+Utility.m
@@ -8,6 +8,10 @@
 
 #import "UIViewController+Utility.h"
 
+#import "NSMutableArray+Utility.h"
+#import "NSObject+Utility.h"
+#import "NSMutableDictionary+Utility.h"
+
 @implementation UIViewController (Eunomia_Utility)
 
 - (void)replaceInSuperviewWithViewController:(UIViewController *)viewController
@@ -65,6 +69,165 @@
     {
         [self replaceRootViewControllerWithViewController:viewController];
     }
+}
+
+@end
+
+NSString *const WeAreAllTextSoAssignUsViewGroupsKey = @"WeAreAllTextSoAssignUsViewGroups";
+
+@interface UIViewController (Eunomia_Utility_WeAreAllTextSoAssignUs_Protected)
+
+@property (strong, readwrite) NSMutableDictionary *viewGroups;
+
+@end
+
+@implementation NSArray (Eunomia_Utility_WeAreAllTextSoAssignUs)
+
++ (NSArray *)arrayOfLabel:(UILabel *)label
+                andButton:(UIButton *)button
+             andTextField:(UITextField *)textField
+              andTextView:(UITextView *)textView
+{
+    NSMutableArray *array = [NSMutableArray array];
+    [array addObjectSafe:label];
+    [array addObjectSafe:button];
+    [array addObjectSafe:textField];
+    [array addObjectSafe:textView];
+    return array;
+}
+
+@end
+
+@implementation UIViewController (Eunomia_Utility_WeAreAllTextSoAssignUs)
+
+- (void)mapViews:(NSArray *)views forKey:(NSString *)key
+{
+    [self.viewGroups setObjectSafe:views forKey:key];
+}
+
+- (void)mapLabel:(UILabel *)label
+       andButton:(UIButton *)button
+    andTextField:(UITextField *)textField
+     andTextView:(UITextView *)textView
+          forKey:(NSString *)key
+{
+    [self mapViews:[NSArray arrayOfLabel:label andButton:button andTextField:textField andTextView:textView]
+            forKey:key];
+}
+
+- (void)callOnViews:(void (^)(NSArray *views) )callBlock forKey:(NSString *)key
+{
+    id viewsId = [self.viewGroups objectForKey:key];
+    if (viewsId && [viewsId isKindOfClass:[NSArray class] ] )
+    {
+        NSArray *views = (NSArray *)viewsId;
+        callBlock( views );
+    }
+}
+
+- (void)setViewGroups:(NSMutableDictionary *)viewGroups
+{
+    [self setProtocolRetainProperty:WeAreAllTextSoAssignUsViewGroupsKey value:viewGroups];
+}
+
+- (NSMutableDictionary *)viewGroups
+{
+    NSMutableDictionary *result;
+    
+    id viewGroupsObject = [self getProtocolProperty:WeAreAllTextSoAssignUsViewGroupsKey];
+    if ( [viewGroupsObject isKindOfClass:[NSMutableDictionary class] ] )
+    {
+        result = viewGroupsObject;
+    } else
+    {
+        // Lazy Create
+        result = [NSMutableDictionary dictionary];
+        self.viewGroups = result;
+    }
+    return result;
+}
+
+
++ (void)setText:(NSString *)text toView:(UIView *)view
+{
+    if ( [view isKindOfClass:[UILabel class] ] )
+    {
+        UILabel *label = (UILabel *)view;
+        label.text = text;
+    } else if ( [view isKindOfClass:[UIButton class] ] )
+    {
+        UIButton *button = (UIButton *)view;
+        button.titleLabel.text = text;
+    } else if ( [view isKindOfClass:[UITextField class] ] )
+    {
+        UITextField *textField = (UITextField *)view;
+        textField.text = text;
+    } else if ( [view isKindOfClass:[UITextView class] ] )
+    {
+        UITextView *textView = (UITextView *)view;
+        textView.text = text;
+    }
+}
+
+- (void)setText:(NSString *)text toViews:(NSArray *)views
+{
+    [views enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        if ( [obj isKindOfClass:[UIView class] ] )
+        {
+            UIView *view = obj;
+            [UIViewController setText:text toView:view];
+        }
+    } ];
+}
+
+- (void)setText:(NSString *)text toViewsWithKey:(NSString *)key
+{
+    [self callOnViews:^(NSArray *views) {
+        [self setText:text toViews:views];
+    } forKey:key];
+}
+
+- (void)setText:(NSString *)text
+        toLabel:(UILabel *)label
+      andButton:(UIButton *)button
+   andTextField:(UITextField *)textField
+    andTextView:(UITextView *)textView
+{
+    [self setText:text
+          toViews:[NSArray arrayOfLabel:label andButton:button andTextField:textField andTextView:textView] ];
+}
+
++ (void)setHidden:(BOOL)hidden toView:(UIView *)view
+{
+    [view setHidden:hidden];
+}
+
+- (void)setHidden:(BOOL)hidden toViews:(NSArray *)views
+{
+    [views enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        if ( [obj isKindOfClass:[UIView class] ] )
+        {
+            UIView *view = obj;
+            [UIViewController setHidden:hidden toView:view];
+        }
+    } ];
+}
+
+- (void)setHidden:(BOOL)hidden toViewsWithKey:(NSString *)key
+{
+    [self callOnViews:^(NSArray *views) {
+        [self setHidden:hidden toViews:views];
+    } forKey:key];
+}
+
+- (void)setHidden:(BOOL)hidden
+          toLabel:(UILabel *)label
+        andButton:(UIButton *)button
+     andTextField:(UITextField *)textField
+      andTextView:(UITextView *)textView
+{
+    [self setHidden:hidden
+            toViews:[NSArray arrayOfLabel:label andButton:button andTextField:textField andTextView:textView] ];
 }
 
 @end
