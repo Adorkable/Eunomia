d84778e3feb11f82b6f54a0f3981ad7569fc3c83
Ian G <yo.ian.g@gmail.com>
2015-02-20 15:50:18 -0500

    UIWebView activity indicator

--- Source/Core/Utility/UIWebView+Utility.h
+++ Source/Core/Utility/UIWebView+Utility.h
@@ -7,9 +7,16 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <JavaScriptCore/JavaScriptCore.h>
+
+@class JSContext;
 
 @interface UIWebView (Eunomia_Utility)
+
+@property (readwrite) IBInspectable BOOL useActivityIndicator;
+@property (readonly) UIActivityIndicatorView *activityIndicator;
+// TODO: swizzle delegate hooks?
+- (void)startedActivity; // manual start and stop for the activity indicator so we don't take over the WebView's delegate
+- (void)stoppedActivity;
 
 - (JSContext *)jsContext;
 - (void)loadFullCanvasHTML;
