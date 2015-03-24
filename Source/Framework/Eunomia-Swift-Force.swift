d45c9069ca0f067215e40bc21f929d4f80bd02b1
Ian G <yo.ian.g@gmail.com>
2015-03-24 16:52:34 -0600

    potential swift framework fix

---
+++ Source/Framework/Eunomia-Swift-Force.swift
@@ -0,0 +1,9 @@
+//
+//  Eunomia-Swift-Force.swift
+//  Eunomia
+//
+//  Created by Ian on 3/24/15.
+//  Copyright (c) 2015 Eunomia. All rights reserved.
+//
+
+import Foundation
--- Source/Framework/Eunomia.h
+++ Source/Framework/Eunomia.h
@@ -14,6 +14,30 @@ FOUNDATION_EXPORT double EunomiaVersionNumber;
 //! Project version string for Eunomia.
 FOUNDATION_EXPORT const unsigned char EunomiaVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Eunomia/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like
+#import "Config.h"
 
+#import "CoreData.h"
 
+#import "Exceptions.h"
+
+#import "Segues.h"
+
+#import "Utility.h"
+
+#import "View+Templating.h"
+
+#import "Views.h"
+
+#import "ThirdPartyWrapper.h"
+
+#import "NSLogWrapper.h"
+#import "VerboseDDLogFormatter.h"
+
+#import "Cocoalumberjack.h"
+#import "CLIColor.h"
+#import "DDAbstractDatabaseLogger.h"
+#import "DDContextFilterLogFormatter.h"
+#import "DDDispatchQueueLogFormatter.h"
+#import "DDLog+LOGV.h"
+#import "DDMultiFormatter.h"
