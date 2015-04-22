f61ebd8654573966cd2f0a8fcc27882a8ce7665a
Ian Grossberg <yo.ian.g@gmail.com>
2015-04-22 00:45:36 -0400

    Empty EunomiaViewController for extensions

---
+++ Source/Core/Views/EunomiaViewController.m
@@ -0,0 +1,13 @@
+//
+//  EunomiaViewController.m
+//  Eunomia
+//
+//  Created by Ian on 4/22/15.
+//  Copyright (c) 2015 Eunomia. All rights reserved.
+//
+
+#import "EunomiaViewController.h"
+
+@implementation EunomiaViewController
+
+@end
