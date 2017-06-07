80e34786b76fd085d38f42244d655e9c7bafab20
Ian G <yo.ian.g@gmail.com>
2017-06-07 16:07:19 -0400

    safety check

--- Source/Core/UtilityExtensions/NSCharacterSet+Utility.swift
+++ Source/Core/UtilityExtensions/NSCharacterSet+Utility.swift
@@ -13,15 +13,17 @@ extension CharacterSet {
     // TODO: ascending and descending
     public var charactersInSetAscending : String {
         var result = String()
-        
-        for test in unichar.min...unichar.max - 1 {
-            if self.contains(UnicodeScalar(test)!) {
-                
-                var found = test
-                withUnsafePointer(to: &found, { (testPointer) -> Void in
-                    let append = NSString(characters: testPointer, length: 1)
-                    result = result + (append as String)
-                })
+
+        for test in unichar.min...unichar.max {
+            if let unicodeValue = UnicodeScalar(test) {
+                if self.contains(unicodeValue) {
+
+                    var found = test
+                    withUnsafePointer(to: &found, { (testPointer) -> Void in
+                        let append = NSString(characters: testPointer, length: 1)
+                        result = result + (append as String)
+                    })
+                }
             }
         }
         
