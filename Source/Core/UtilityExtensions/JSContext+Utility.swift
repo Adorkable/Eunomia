4dbb8a8fa2c5073360882b34053255aaf98f8523
Ian Grossberg <yo.ian.g@gmail.com>
2018-11-10 14:45:37 -0500

    Disabling until fixed with Carthage building

--- Source/Core/UtilityExtensions/JSContext+Utility.swift
+++ Source/Core/UtilityExtensions/JSContext+Utility.swift
@@ -10,12 +10,12 @@ import JavaScriptCore
 
 extension JSContext {
 
-    public func setConsoleLogHandler(_ handler : @escaping @convention(block)(String) -> String) {
-        self.setObject(unsafeBitCast(handler, to: AnyObject.self), forKeyedSubscript: "log" as NSCopying & NSObjectProtocol)
-        
-        // TODO: test if console already exists before creating each time
-        self.evaluateScript("var console = new Object();")
-        
-        self.objectForKeyedSubscript("console").setObject(unsafeBitCast(handler, to: AnyObject.self), forKeyedSubscript: "log" as NSCopying & NSObjectProtocol)
-    }
+//    public func setConsoleLogHandler(_ handler : @escaping @convention(block)(String) -> String) {
+//        self.setObject(unsafeBitCast(handler, to: AnyObject.self), forKeyedSubscript: "log" as NSCopying & NSObjectProtocol)
+//        
+//        // TODO: test if console already exists before creating each time
+//        self.evaluateScript("var console = new Object();")
+//        
+//        self.objectForKeyedSubscript("console").setObject(unsafeBitCast(handler, to: AnyObject.self), forKeyedSubscript: "log" as NSCopying & NSObjectProtocol)
+//    }
 }
